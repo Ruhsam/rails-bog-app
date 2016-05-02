b5a17770c3a638423be012f53c127fc8338cc388
ruhsam <ruhsam@gmail.com>
2016-05-02 07:52:17 -0700

    all rest routes working

--- bog_app2/app/controllers/creatures_controller.rb
+++ bog_app2/app/controllers/creatures_controller.rb
@@ -23,8 +23,27 @@ class CreaturesController < ApplicationController
    end
 
    def show
-    creature_id = params[:id]
-    @creature = Creature.find_by_id(creature_id)
-    render :show
- end
+      creature_id = params[:id]
+      @creature = Creature.find_by_id(creature_id)
+      render :show
+   end
+   def edit
+      creature_id = params[:id]
+      @creature = Creature.find_by_id(creature_id)
+      render :edit
+   end
+   def update
+      creature_id = params[:id]
+      creature = Creature.find_by_id(creature_id)
+      creature_params = params.require(:creature).permit(:name, :description)
+      creature.update_attributes(creature_params)
+      redirect_to creature_path(creature)
+   end
+   def destroy
+      creature_id = params[:id]
+      creature = Creature.find_by_id(creature_id)
+      creature.destroy
+      redirect_to creatures_path
+   end
+
 end
