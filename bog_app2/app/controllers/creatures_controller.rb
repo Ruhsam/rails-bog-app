be3e6a2972a4475f56dee124712b0652d3b0eb6b
ruhsam <ruhsam@gmail.com>
2016-05-02 07:22:34 -0700

    new monster route completed

--- bog_app2/app/controllers/creatures_controller.rb
+++ bog_app2/app/controllers/creatures_controller.rb
@@ -8,4 +8,17 @@ class CreaturesController < ApplicationController
       @creatures = Creature.all
       render :index
    end
+
+   def new
+      @creature = Creature.new
+      render :new
+   end
+
+   def create
+      creature_params = params.require(:creature).permit(:name, :description)
+      creature = Creature.new(creature_params)
+      if creature.Save
+         redirect_to creature_path
+      end
+   end
 end
